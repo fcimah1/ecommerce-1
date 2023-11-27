0e2e8f5be30b77ff3a3d2969e9587a8f24878001
fcimah1 <eng.mohamedahmed2741991@gmail.com>
2023-11-27 03:56:53 +0200

    update category

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import "./category.css"
 export default function Categories() {
     const [category, setCategory] = useState([])
     useEffect(() => {
@@ -12,7 +12,7 @@ export default function Categories() {
         <ul className="dropdown-menu">
             {
                 category.map((categ, id) => {
-                    return (<li key={id}><Link key={id} className="dropdown-item" to={`category/${categ}`}>{categ}</Link></li>)
+                    return (<li key={id}><Link key={id} className="dropdown-item category" to={`category/${categ}`}>{categ}</Link></li>)
                 })
             }
         </ul>
